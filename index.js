30f60b7bc55bff87e1595791250a4df4b788dd64
Reham Galal Abd El-aziz <85314647+rehamgalal56@users.noreply.github.com>
2025-04-25 23:18:46 +0300

    CreategetCurrentUser

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/auth.routes");
+const userRoutes = require("./routes/user.routes");
 const projectRoutes = require("./routes/project.routes");
 const sectionRoutes = require("./routes/Section.routes");
 
@@ -21,6 +22,7 @@ mongoose
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/sections", sectionRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(process.env.PORT, () => {
   logger(`Server running on port ${process.env.PORT}`);
